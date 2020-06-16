ccdba348b8d7626272b1c22b7340495a6b0936b9
karlaVane <kmoyonr@est.ups.edu.ec>
2020-06-16 17:19:57 -0500

    Aumento de presión atmosférica y humedad

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const clima = require('./controlador/clima')
+const colors = require("colors");
 const argv = require('yargs').options({
     ciudad: {
         alias: 'c',
         desc: 'nombre de la ciudad para obtener el clima',
         demand: true
+    },
+    opciones: {
+        alias: 'o',
+        desc: 'opciones para consultar: p -> La presión atmosférica, h -> humedad'
     }
 }).argv
 
-console.log(argv.ciudad);
-//clima.getClima(argv.ciudad)
-//    .then(console.log).catch(console.log); //imprimo lo q devuelve
 
+let mensaje
 const getInformacion = async(ciudad) => {
     try {
-        const temp = await clima.getClima(argv.ciudad);
-        return `El clima de ${ciudad} es de ${temp}`;
+        const data = await clima.getClima(argv.ciudad);
+        mensaje = "---------------------------------------\n".rainbow
+        mensaje += `Ciudad: `.cyan + `${ciudad}`.toUpperCase() + `\nTemperatura: `.cyan + `${data.temp} ° centígrados\n`;
+        if (argv.opciones == 'h') {
+            mensaje += `Humedad: `.cyan + `${data.humedad}\n`
+        } else if (argv.opciones == 'p') {
+            mensaje += `Presión: `.cyan + `${data.presion}\n`
+        }
+        mensaje += "---------------------------------------".rainbow
+        return mensaje
     } catch (e) {
-        return `No se pudo obtener el clima de ${ciudad} ° centígrados`
+        return `No se pudo obtener información de ${ciudad}.`
     }
 }
 
--- controlador/clima.js
+++ controlador/clima.js
@@ -2,9 +2,12 @@ const axios = require('axios'); //libreria axios: peticiones http devuelve una p
 const getClima = async(ciudad) => {
     const ciudadURL = encodeURI(ciudad); //codificar para q se pueda utilizar en la URL 
     const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudadURL}&appid=59c4794622a7784d65073545da0ce0ef&units=metric`)
-    return resp.data.main.temp;
+        //en resp se guardda todo el json
+    const temp = resp.data.main.temp;
+    const humedad = resp.data.main.humidity;
+    const presion = resp.data.main.pressure;
+    return { temp, humedad, presion };
 }
 module.exports = {
-        getClima
-    }
-    //en resp se guardda todo el json
+    getClima
+}
